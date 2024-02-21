c2a9b903689910d5a70f194508dce3124979bb72
egarcia0992 <78242213+egarcia0992@users.noreply.github.com>
2024-02-21 10:18:46 -0600

    UI Update 1

--- Screens/JournalScreen.js
+++ Screens/JournalScreen.js
@@ -3,7 +3,17 @@ import { Pressable, StyleSheet, Text, View} from "react-native";
 const JournalScreen = ({ navigation }) => {
   return (
     <View style={{flex: 1}}>
-      <Text>This is Journal Screen...</Text>
+      <View style={styles.journalRow}>
+        <Text style={styles.journalDate}>
+          Sep 23, 24
+        </Text>
+      </View>
+      <View style={styles.journalRow}>
+        <Text style={styles.journalDate}>
+          Sep 25, 24
+        </Text>
+      </View>
+
       <View style={styles.container}>
       <Pressable style={styles.journalButton}>
         <Text style={styles.buttonText}>+</Text>
@@ -37,5 +47,18 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: "bold",
     transform: [{ translateX: 0 }, { translateY: -2 }],
+  },  
+  journalRow: {
+    margin: 5,
+    borderRadius: 20,
+    backgroundColor: "lightgrey",
+    alignItems: "center",
   },
+  journalDate: {
+    fontSize: 25,
+    borderRadius: 20,
+    padding: 5,
+    margin: 5,
+  },
+
 });
