77de1f3edf995d502ce0c24a0bc14403515038db
bsester <124089310+bsester@users.noreply.github.com>
2024-04-20 13:13:44 -0500

    account deletion
    added support for account deletion
    this took way longer than expected

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,11 +1,15 @@
-import {StyleSheet, Text, TouchableOpacity, View} from "react-native";
-import React, {useEffect} from "react";
+import {Alert, Modal, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import React, {useEffect, useState} from "react";
 import {auth} from "../firebase";
 import {useNavigation} from "@react-navigation/native";
+import text from "react-native-web/src/exports/Text";
+import {EmailAuthProvider} from "@firebase/auth";
 
 const ProfileScreen = ({ navigation, route }) => {
 
   // if user is not logged in, redirect them to the login page
+  const [ModalVisible, setModalVisible] = useState(false);
+  const [passwordInput, setPasswordInput] = useState("");
   const myNav = useNavigation();
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user =>
@@ -33,11 +37,28 @@ const ProfileScreen = ({ navigation, route }) => {
   {
       navigation.replace("PasswordChange");
   }
+  const handleDeletion = () => // helper function to prompt user
+  {
+      setModalVisible(true);
+  }
+  const deleteAccount = (password) =>{
+    // verifying password
+    const user = auth.currentUser;
+    const userCredentials = EmailAuthProvider.credential(user.email, password);
+    user.reauthenticateWithCredential(userCredentials)
+        .then(() =>{ // correct password, proceed with deletion
+            user.delete().then(() => {console.log("User deleted successfully.")})
+        }).catch((error)=>{console.log(error.message)
+                          alert("Incorrect Password")})
+        .catch((error) => alert("Incorrect Password.")) // incorrect password
+  }
   return (
       <View>
-        <Text>
-          Hello {auth.currentUser?.email}!
-        </Text>
+        <View style>
+          <Text style = {styles.greetingText}>
+            Hello {auth.currentUser?.email}!
+          </Text>
+        </View>
         <View
           style={styles.buttonContainer}>
           <TouchableOpacity
@@ -50,7 +71,40 @@ const ProfileScreen = ({ navigation, route }) => {
             style={[styles.button, styles.buttonOutline]}>
           <Text style ={styles.buttonOutLineText}> Log Out </Text>
         </TouchableOpacity>
-
+            <TouchableOpacity onPress={handleDeletion} style={[styles.button, styles.buttonOutline]}>
+                <Text style={styles.buttonOutLineText}> Delete Account </Text>
+            </TouchableOpacity>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={ModalVisible}
+                onRequestClose={() => setModalVisible(false)}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Text style={styles.modalText}>Enter your password to confirm account deletion. (This cannot be undone).</Text>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Password"
+                            secureTextEntry={true}
+                            onChangeText={text => setPasswordInput(text)}
+                        />
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity
+                                onPress={() => {
+                                    setModalVisible(false);
+                                    deleteAccount(passwordInput);
+                                }}
+                                style={[styles.button, styles.buttonConfirm]}>
+                                <Text style={styles.buttonText}>Confirm</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => {setModalVisible(false)}} style={[styles.button, styles.buttonCancel]}>
+                                <Text style={styles.buttonText}>Cancel</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </View>
       </View>
   );
@@ -66,12 +120,23 @@ const styles = StyleSheet.create({
   },
   button: {
     backgroundColor: 'azure',
-    width: '100%',
+    width: '40%',
     padding: 15,
     borderRadius: 10,
     alignItems: 'center'
   },
-  buttonOutline: {
+  buttonContainer: {
+    alignItems: 'center'
+  },
+  buttonConfirm: {
+      width: '80%',
+      backgroundColor: 'green'
+  },
+  buttonCancel: {
+      width: '80%',
+      backgroundColor: 'red'
+  },
+    buttonOutline: {
     backgroundColor: 'white',
     marginTop: 5,
     borderColor: 'azure',
@@ -79,6 +144,38 @@ const styles = StyleSheet.create({
   },
   buttonOutlineText: {
     fontWeight: '700',
-    fontSize: 16
-  }
+    fontSize: 16,
+    fontFamily: "RedHatMono"
+  },
+  centeredView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 22,
+  },
+  greetingText: {
+    fontSize: 20,
+    fontFamily: "BoldRedHatMono",
+    marginBottom: 20,
+    textAlign: 'center'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+       width: 0,
+       height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+    },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
 });
