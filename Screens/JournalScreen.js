25e47dedb278f7fac45442be15d4b2f83eaee4ca
bsester <124089310+bsester@users.noreply.github.com>
2024-04-20 19:58:31 -0500

    journal recording
    added useeffect to grab user journals

--- Screens/JournalScreen.js
+++ Screens/JournalScreen.js
@@ -1,27 +1,35 @@
 import { Pressable, StyleSheet, Text, View} from "react-native";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {useNavigation} from "@react-navigation/native";
 import {auth} from "../firebase";
+import firebase from "firebase/compat";
 
 const JournalScreen = ({ navigation }) => {
+  const [journals, setJournals] = useState([]);
+  const [currJournal, setCurrJournal] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user =>
     {
-      if (!user)
-      {
+      if (!user){
         alert('Please log in before accessing your journals.');
         navigation.replace("LogIn");
       }
-      else
-      {
-        const userToken = user.getIdToken().then(token => {
-          // firestore stuff here
-        }).catch(error => console.log(error.message));
-      }
-
     })
     return unsubscribe;
   })
+  useEffect(() => {
+    const user = auth.currentUser;
+    const unsubscribe = firebase.firestore()
+        .collection("users").doc(user.uid)
+        .collection("journalEntries").onSnapshot(snapshot => {
+          const journalData = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}));
+          setJournals(journalData);
+        })
+
+  }, []);
   return (
       <View style={{flex: 1}}>
         <View style={styles.journalRow}>
